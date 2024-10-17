6aa4c94b94f67bef71255665e48907bf25c578fb
Nicholas Angeles <blackdeath546@gmail.com>
2024-10-17 18:03:29 -0400

    Added Stiff Code

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+            Console.WriteLine(response);
+            // if (answer < 9)
+            // {
+            //     response = $"{answer} is less than nine";
+            //     Console.WriteLine(response);
+            // }
+            // else
+            // {
+            //     response = $"{answer} is greater than or equal to nine";
+            //     Console.WriteLine(response);
+            // }
         }
     }
 }
